5688a85eba5b0356877f8ca39ab76637f28e98bf
henrique <henriquebrendonti26@gmail.com>
2025-04-30 16:52:30 -0300

    Adiciona SLF4J para rastrear chamadas de API e erros

--- src/main/java/com/backend/formulario/util/CepUtil.java
+++ src/main/java/com/backend/formulario/util/CepUtil.java
@@ -9,27 +9,44 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class CepUtil implements Serializable {
     
     public static String consultarCep(String cep) throws IOException  {
+        log.info("Iniciando consulta para o CEP: {}", cep);
+
         String encodedCep = URLEncoder.encode(cep, StandardCharsets.UTF_8.toString());
         String urlString = String.format("https://viacep.com.br/ws/%s/json", encodedCep);
+
+        log.debug("URL da requisição: {}", urlString);
 
         URL url = new URL(urlString);
         HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
 
+        int statusCode = conexao.getResponseCode();
         if(conexao.getResponseCode() != 200) {
+            log.error("Falha na requisição para o CEP {}. Código de status HTTP: {}", cep, statusCode);
             throw new IOException("Falha na requisição ao consultar o CEP.");
         }
 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(conexao.getInputStream(), StandardCharsets.UTF_8))) {
-            StringBuilder respota = new StringBuilder();
+            StringBuilder resposta = new StringBuilder();
             String inputLine;
 
             while((inputLine = br.readLine())!= null) {
-                respota.append(inputLine);
+                resposta.append(inputLine);
             }
-            return respota.toString();
+
+            log.info("Consulta ao CEP {} concluída com sucesso.", cep);
+            log.debug("Resposta da API: {}", resposta.length() > 300 ? resposta.substring(0, 300) + "..." : resposta.toString());
+            
+            return resposta.toString();
+
+        } catch(IOException e) {
+            log.error("Erro ao ler a resposta da API para o CEP {}: {}", cep, e.getMessage(), e);
+            throw e;
         }
     }
 }
