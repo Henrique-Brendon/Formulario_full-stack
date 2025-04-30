7ed47df964248fb0170c5fdb03e9072a7a57736c
henrique <henriquebrendonti26@gmail.com>
2025-04-30 17:24:08 -0300

    Cria ErrorResponse e trata exceções globais com @ControllerAdvice

---
+++ src/main/java/com/backend/formulario/controllers/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.backend.formulario.controllers.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.backend.formulario.util.exceptions.CepFormatoInvalidoException;
+import com.backend.formulario.util.exceptions.CepNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<ErrorResponse> handlerNullPointerException(NullPointerException ex, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+            HttpStatus.BAD_REQUEST.value(), "Null Pointer Exception",
+            "Um valor nulo foi encontrado onde não era esperado.", request.getRequestURI()
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CepNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleCepNotFoundException(CepNotFoundException ex, HttpServletRequest request) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                "CEP não encontrado",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(CepFormatoInvalidoException.class)
+    public ResponseEntity<ErrorResponse> handleCepFormatoInvalidoException(
+            CepFormatoInvalidoException ex, HttpServletRequest request) {
+
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "CEP inválido",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+}
