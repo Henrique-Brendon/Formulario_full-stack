7fbba4e9628c325847f89c44868c7099a0b1697b
henrique brendon <henriquebrendonti26@gmail.com>
2025-05-01 12:05:24 -0300

    Adiciona mapeamento de persistência em Usuario e CepInfo

--- src/main/java/com/backend/formulario/entities/CepInfo.java
+++ src/main/java/com/backend/formulario/entities/CepInfo.java
@@ -1,22 +1,28 @@
 package com.backend.formulario.entities;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-@EqualsAndHashCode  
 @AllArgsConstructor
 @Getter
 @Setter
 @Builder
+@Table("ENDERECO")
 public class CepInfo {
+    
+    @Id
     private Long id;
-    String cep;
-    String estado;
-    String cidade;
-    String bairro;
-    String endereco;
-    int numCasa;
+    private String cep;
+    private String estado;
+    private String cidade;
+    private String bairro;
+    private String endereco;
+    private int numCasa;
+
+    private Long usuarioId;
 }
--- src/main/java/com/backend/formulario/entities/Usuario.java
+++ src/main/java/com/backend/formulario/entities/Usuario.java
@@ -2,22 +2,26 @@ package com.backend.formulario.entities;
 
 import java.time.Instant;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-@EqualsAndHashCode  
 @AllArgsConstructor
 @Getter
 @Setter
 @Builder
+@Table("USUARIOS")
 public class Usuario {
     
+    @Id
     private Long id;
     private String nome;
     private Instant dataNascimento;
     private String email;
     private String senha;
+    
 }
