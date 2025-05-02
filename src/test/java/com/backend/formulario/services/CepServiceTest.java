96273b273803215693438789c830ca94c9f74aac
henrique <henriquebrendonti26@gmail.com>
2025-05-02 15:54:55 -0300

    Test(CepService): implementa verificação de conversão de JSON em CepDTO

---
+++ src/test/java/com/backend/formulario/services/CepServiceTest.java
@@ -0,0 +1,43 @@
+package com.backend.formulario.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.backend.formulario.controllers.dtos.CepDTO;
+
+@ExtendWith(MockitoExtension.class)
+public class CepServiceTest {
+    
+    private CepService cepService;
+
+    @BeforeEach
+    void setUp() {
+        cepService = new CepService();
+    }
+
+    @Test
+    void deveRetornarCepDTO_QuandoJsonValidoInformado() {
+        String jsonValido = """
+            {
+                "cep": "05716-070",
+                "uf": "SP",
+                "localidade": "São Paulo",
+                "bairro": "Vila Andrade",
+                "logradouro": "Rua Itacaiúna"
+            }
+        """;
+
+        CepDTO dto = cepService.verificarEndereco(jsonValido);
+
+        assertNotNull(dto);
+        assertEquals("05716-070", dto.cep());
+        assertEquals("SP", dto.estado());
+        assertEquals("São Paulo", dto.cidade());
+        assertEquals("Vila Andrade", dto.bairro());
+        assertEquals("Rua Itacaiúna", dto.endereco());
+    }
+}
