b7cc028586c30ea0f90856d81dffe7008f176bc4
henrique <henriquebrendonti26@gmail.com>
2025-05-05 09:28:32 -0300

    Adiciona testes de integração para cenários de erro com CEP inválido e não encontrado

--- src/test/java/com/backend/formulario/integration/CepIt.java
+++ src/test/java/com/backend/formulario/integration/CepIt.java
@@ -35,6 +35,34 @@ public class CepIt {
         assertEquals("Vila Andrade", response.getBody().bairro());
         assertEquals("Rua Itacaiúna", response.getBody().endereco());
     }
+
+    @Test
+    void deveRetornar404_QuandoCepNaoEncontrado() {
+        String cepInexistente = "00000-000";
+        String url = "http://localhost:" + port + "/cep/localizarEndereco?cep=" + cepInexistente;
     
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+    
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertTrue(response.getBody().contains("\"status\":404"));
+        assertTrue(response.getBody().contains("\"message\":\"CEP não encontrado: " + cepInexistente + "\""));
+        assertTrue(response.getBody().contains("\"path\":\"/cep/localizarEndereco\""));
+    }
+    
+
+    @Test
+    void deveRetornar400_QuandoFormatoDeCepForInvalido() {
+        String cepInvalido = "000000-000";
+        String url = "http://localhost:" + port + "/cep/localizarEndereco?cep=" + cepInvalido;
+    
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+    
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertTrue(response.getBody().contains("\"status\":400"));
+        assertTrue(response.getBody().contains("\"message\":\"Formato de CEP inválido: " + cepInvalido + "\""));
+        assertTrue(response.getBody().contains("\"path\":\"/cep/localizarEndereco\""));
+    }
 
 }
